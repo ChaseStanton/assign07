4ee7b1a08afa9c11fc37d3a8325d1a8c280c982c
Reece Kalmar <ralmarkalmar@gmail.com>
2023-10-27 19:21:35 -0600

    s

--- TimingExperiment.java
+++ TimingExperiment.java
@@ -6,63 +6,47 @@ import java.util.List;
 public class TimingExperiment {
 
     public static void main(String[] args) {
-        int startN = 100;
-        int endN = 1000;
-        int stepSize = 100;
+        int startN = 10;
+        int endN = 100;
+        int stepSize = 10;
 
         System.out.println("N\tAreConnected(ns)\tShortestPath(ns)\tSort(ns)");
 
         for (int N = startN; N <= endN; N += stepSize) {
-            Graph<String> graph = createGraph(N);
+            ArrayList<String> sources = new ArrayList<>();
+            ArrayList<String> destinations = new ArrayList<>();
 
-            long areConnectedTime = measureAreConnected(graph);
-            long shortestPathTime = measureShortestPath(graph);
-            long sortTime = measureSort(graph);
+            for (int i = 0; i < N; i++) {
+                String source = "Node" + i;
+                String destination = "Node" + (i + 1);
+
+                sources.add(source);
+                destinations.add(destination);
+
+            }
+            String srcData = "Node0";
+            String dstData = "Node" + (GraphUtility.sort(sources, destinations).size() - 1);
+
+            long startTime1 = System.nanoTime();
+            GraphUtility.areConnected(sources, destinations, srcData, dstData);
+            long stopTime1 = System.nanoTime();
+
+            long areConnectedTime = stopTime1 - startTime1;
+
+            long startTime2 = System.nanoTime();
+            GraphUtility.shortestPath(sources, destinations, srcData, dstData);
+            long stopTime2 = System.nanoTime();
+
+            long shortestPathTime = stopTime2 - startTime2;
+
+            long startTime3 = System.nanoTime();
+            GraphUtility.sort(sources, destinations);
+            long stopTime3 = System.nanoTime();
+
+            long sortTime = stopTime3 - startTime3;
 
             System.out.println(N + "\t" + areConnectedTime + "\t" + shortestPathTime + "\t" + sortTime);
         }
     }
 
-    private static Graph<String> createGraph(int N) {
-        Graph<String> graph = new Graph<>();
-        ArrayList<String> sources = new ArrayList<>();
-        ArrayList<String> destinations = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
-            String source = "Node" + i;
-            String destination = "Node" + (i + 1);
-
-            sources.add(source);
-            destinations.add(destination);
-
-            graph.addEdge(source, destination);
-        }
-
-        return graph;
-    }
-
-    private static long measureAreConnected(Graph<String> graph) {
-        String srcData = "Node0";
-        String dstData = "Node" + (graph.sortVertices().size() - 1);
-        long startTime = System.nanoTime();
-        boolean areConnected = graph.areConnected(srcData, dstData);
-        long endTime = System.nanoTime();
-        return (endTime - startTime);
-    }
-
-    private static long measureShortestPath(Graph<String> graph) {
-        String srcData = "Node0";
-        String dstData = "Node" + (graph.sortVertices().size() - 1);
-        long startTime = System.nanoTime();
-        List<String> shortestPath = graph.shortestPath(srcData, dstData);
-        long endTime = System.nanoTime();
-        return (endTime - startTime);
-    }
-
-    private static long measureSort(Graph<String> graph) {
-        long startTime = System.nanoTime();
-        List<String> sortedVertices = graph.sortVertices();
-        long endTime = System.nanoTime();
-        return (endTime - startTime);
-    }
 }
